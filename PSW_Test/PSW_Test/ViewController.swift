58131822d7b7c85b1f150f592e228af4299c56b8
Daniil Burnashov <danil-burnashov@mail.ru>
2023-03-25 00:26:35 +0300

    init main

---
+++ PSW_Test/PSW_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PSW_Test
+//
+//  Created by Daniil on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
